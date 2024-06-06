b5be28a5c83df7dbe74115f98aec848c1e97ace6
Ghileslim <slim.ghi3@gmail.com>
2024-06-06 03:32:23 +0100

    Ajout de la configuration pour l'extension Wiflix.

---
+++ Plugin.kt
@@ -0,0 +1,13 @@
+val version = 1
+
+cloudstream {
+    language = "fr"
+    description = "WIFLIX, le site grâce auquel vous allez pouvoir regarder vos films et séries préférées"
+    authors = listOf("Votre nom")
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie"
+    )
+    val baseUrl = "http://wiflix.promo"
+}
